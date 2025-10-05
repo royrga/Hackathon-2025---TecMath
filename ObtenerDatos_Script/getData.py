76861180e2eaf779c3b7a755dc8cc20b0c80edd3
royrga <royrga@gmail.com>
2025-10-04 22:52:14 -0600

    Fix missing comma

--- ObtenerDatos_Script/getData.py
+++ ObtenerDatos_Script/getData.py
@@ -9,7 +9,8 @@ results = earthaccess.search_data(
     short_name="M2T1NXSLV",
     version="5.12.4",
     temporal=(
-        "1980-01-01" "1980-01-01",
+        "1980-01-01",
+        "1980-01-01",
     ),  # This will stream one granule, but can be edited for a longer temporal extent
     bounding_box=(-180, 0, 180, 90),  # A tuple representing spatial bounds in the form
     # `(lower_left_lon, lower_left_lat, upper_right_lon, upper_right_lat)`
